f5aeb4904b4ec2fec6b286bfaae7888572abc59b
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-02-25 14:52:09 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+// changing the title of the page
+document.title='xyz';
+
+// changing the header title of the page
+var headerTitle = document.getElementById('header-title');
+headerTitle.textContent='NSR';
+headerTitle.innerText='Hello';
+headerTitle.innerHTML='NSINGHRAJPUT';
+headerTitle.style.border='solid 3px #000'
+
+var itemTitle=document.getElementById('itemTitle');
+itemTitle.style.fontWeight='bold';
+itemTitle.style.color='green'
